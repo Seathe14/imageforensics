d14991fbe5a50d6feb6d94fb63c6e38b97a3034d
Valery Sychev <seathe14@gmail.com>
2022-05-23 01:22:08 +0300

    removed comments

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-#import keras
-import tensorflow.python.keras.models
+from keras.models import load_model
 from keras.utils.np_utils import to_categorical # used for converting labels to one-hot-encoding
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.optimizers import Adam
-from sklearn.utils import shuffle
-#from scipy import stats
-#from sklearn.preprocessing import LabelEncoder
 from PIL import Image, ImageChops, ImageEnhance
 import numpy as np
 import os
 import random
-
 
 def convert_to_ela_image(path, quality):
     temp_filename = 'temp_file.jpg'
@@ -55,7 +50,6 @@ def createAndTrainModel():
     X = []  # ELA converted images
     Y = []  # 0 for fake, 1 for real
 
-    count = 0
     path = 'C:/Users/imynn/Downloads/CASIA2/Au'
     for dirname, _, filenames in os.walk(path):
         for filename in filenames:
@@ -78,21 +72,12 @@ def createAndTrainModel():
     print(len(X), len(Y))
 
     path = 'C:/Users/imynn/Downloads/CASIA2/Tp'
-    count = 0
     for dirname, _, filenames in os.walk(path):
         for filename in filenames:
-            #         count += 1
-            #         if count < 1000:
-            #             pass
-            if filename.endswith('jpg') or filename.endswith('png'):
+            if filename.endswith('jpg') or filename.endswith('bmp'):
                 full_path = os.path.join(dirname, filename)
                 X.append(prepare_image(full_path))
                 Y.append(0)
-            #if len(Y) % 2000 == 0:
-             #   print(f'Processing {len(Y)} images')
-              #  break
-       # if len(Y) % 1700 == 0:
-          #  break
 
     print(len(X), len(Y))
 
@@ -135,54 +120,14 @@ def predict_image(path):
     imageCheck = imageCheck.reshape(-1, 128, 128, 3)
     y_pred = model.predict(imageCheck)
     y_pred_class = np.argmax(y_pred, axis = 1)[0]
+    print(y_pred)
     print(f'Class : {class_names[y_pred_class]} Confidence: {np.amax(y_pred) * 100:0.2f}')
 
 if __name__ == '__main__':
     if os.path.isdir(imgTmpModel):
         print('load')
-        model = tensorflow.keras.models.load_model(imgTmpModel)
+        model = load_model(imgTmpModel)
     else:
         print('train')
         model = createAndTrainModel()
-    #model = createAndTrainModel()
-    #print("AA")
-    #real_image_path = 'G:\\tp1.jpg'
-    #Image.open(real_image_path)
-    #imageCheck = prepare_image('C:\\Users\\imynn\\Downloads\\CASIA2\\Tp\\Tp_D_CRN_S_N_cha00029_art00013_00501.tif')
-    #imageCheck = imageCheck.reshape(-1,128,128,3)
-    #y_pred = model.predict(imageCheck)
-    #print(y_pred)
-    #imageCheck = prepare_image('C:\\Users\\imynn\\Downloads\\CASIA2\\Au\\Au_ani_101899.jpg')
-    #imageCheck = imageCheck.reshape(-1,128,128,3)
-    #y_pred = model.predict(imageCheck)
-    #print(y_pred)
-    #ab = convert_to_ela_image(real_image_path, 85)
-    #ab.save("abc.jpg", 'JPEG')
-    #predict_image('C:\\Users\\imynn\\Downloads\\CASIA2\\Tp\\Tp_D_CNN_M_B_nat10139_nat00097_11948.jpg')
     predict_image('C:\\Users\\imynn\\Downloads\\CASIA2\\Au\\Au_ani_101899.jpg')
-    #path = 'C:/Users/imynn/Downloads/CASIA2/Au'
-    #correct_r = 0
-   # total_r = 0
-    #for dirname, _, filenames in os.walk(path):
-    #    for filename in filenames:
-    #        #         count+=1
-    #        #         if count < 1000:
-    #        #             pass
-    #        if filename.endswith('jpg') or filename.endswith('png') or filename.endswith('tif'):
-    #            imageCheck = prepare_image(path + '/' + filename)
-    #            imageCheck = imageCheck.reshape(-1, 128, 128, 3)
-    #            y_pred = model.predict(imageCheck)
-    #            y_pred_class = np.argmax(y_pred, axis=1)[0]
-    #            total_r += 1
-    #            if y_pred_class == 1:
-    #                correct_r +=1
-        #  if len(Y) % 1000 == 0:
-        #     print(f'Processing {len(Y)} images')
-        #    break
-        # if len(Y) % 1000 == 0:
-        #    break
-    #correct += correct_r
-    #total += total_r
-    #print(f'Total: {total_r}, Correct: {correct_r}, Acc: {correct_r / total_r * 100.0}')
-    #print(f'Total: {total}, Correct: {correct}, Acc: {correct / total * 100.0}')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
